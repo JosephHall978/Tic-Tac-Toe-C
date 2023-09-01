c121a448a4ee8c02f7d12b7e242cc7e74f1b1dec
Average_Joe978 <76209909+JosephHall978@users.noreply.github.com>
2023-09-01 14:57:51 +0100

    Framed up cardGame module

---
+++ cardGame.c
@@ -0,0 +1,28 @@
+//
+// Created by josep on 01/09/2023.
+//
+
+int generateDeck(char* deck, int jokers){
+    //generate playable deck
+    return 1;
+}
+
+int showDeck(char* deck){
+    //show each card must include face and suite
+    return 1;
+}
+
+int shuffleDeck(char* deck, int deckSize){
+    //take deck and shuffle cards
+    return 1;
+}
+
+int main(){
+    //generate deck
+
+    //show deck
+
+    //shuffle deck
+
+    //show deck
+}
