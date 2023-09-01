220d1d9feb5e6063db6baf053e81b22f1ce2b6e0
Average_Joe978 <76209909+JosephHall978@users.noreply.github.com>
2023-09-01 11:31:03 +0100

    Created user interface to allow players to select game or exit program

--- main.c
+++ main.c
@@ -2,5 +2,29 @@
 #include "ticTacToe.h"
 
 int main(){
-    ticTacToe();
+    int option;
+    int playing=1;
+    while(playing) {
+        printf("Fun games Written in C options below\n"
+               "\t1. Tic Toe Game\n"
+               "\t2. Card Game\n"
+               "\t3. Quit\n"
+               "Select option: \n");
+        scanf("%d", &option);
+        scanf("%*c");
+        switch (option) {
+            case 1:
+                ticTacToe();
+                break;
+            case 2:
+                printf("under development\n");
+                break;
+            case 3:
+                printf("Exiting games\n");
+                playing = 0;
+                break;
+            default:
+                printf("Invalid option\n");
+        }
+    }
 };
